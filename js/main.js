9c7bdc572db615fba5c2c5cd90055958b8b7e45c
Sharky <Sharky121@mail.ru>
2021-10-15 03:11:40 +0300

    Подготовка данных

--- js/main.js
+++ js/main.js
@@ -1,34 +1,89 @@
-const getRandomInteger = (min = 0, max = 1) => {
-  if (min < 0 || max < 0) {
-    throw new Error('Числа должны быть положительными');
-  }
+const QUANTITY = 10;
 
-  if (min === max) {
-    return min;
-  }
+// Получает случайное целое число
+const getRandomPositiveInteger = (firstNumber, secondNumber) => {
+  const lower = Math.ceil(Math.min(Math.abs(firstNumber), Math.abs(secondNumber)));
+  const upper = Math.floor(Math.max(Math.abs(firstNumber), Math.abs(secondNumber)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-  if (max < min) {
-    [min, max] = [max, min];
-  }
-
-  return Math.round(Math.random() * (max - min + 1) + min);
+  return Math.floor(result);
 };
 
-const getRandomFloat = (min = 0, max = 1, floating = 1) => {
-  if (min < 0 || max < 0) {
-    throw new Error('Числа должны быть положительными');
-  }
+// Получает случайное число с плавающей точкой
+const getRandomPositiveFloat = (firstNumber, secondNumber, digits = 1) => {
+  const lower = Math.min(Math.abs(firstNumber), Math.abs(secondNumber));
+  const upper = Math.max(Math.abs(firstNumber), Math.abs(secondNumber));
+  const result = Math.random() * (upper - lower) + lower;
 
-  if (min === max) {
-    return min;
-  }
-
-  if (max < min) {
-    [min, max] = [max, min];
-  }
-
-  return (Math.random() * (max - min + 1) + min).toFixed(floating);
+  return result.toFixed(digits);
 };
 
-getRandomInteger(2, 10);
-getRandomFloat(1.5, 9.9);
+// Получает случайное значение из массива
+const getRandomValueFromArray = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+
+// Получает массив случайной длины из массива
+const getRandomArray = (array) => array.filter(() => getRandomPositiveInteger(0, 1));
+
+// Создаёт массив случайной длины из массива
+const generateRandomArray = (array) => new Array(getRandomPositiveInteger(1, 10))
+  .fill(null)
+  .map(() => array[getRandomPositiveInteger(0, array.length - 1)]);
+
+// Получает массив названий
+const getTextArray = (string) => (new Array(QUANTITY)
+  .fill(null)
+  .map((el, index) => string + index + 1)
+  .sort(() => Math.random() - 0.5));
+
+// Получат массив путей для фотографий
+const getAvatarSrcArray = () => new Array(QUANTITY)
+  .fill(null)
+  .map((el, index) => `img/avatars/user${index}.png`)
+  .sort(() => Math.random() - 0.5);
+
+const Offers = {
+  AVATAR: getAvatarSrcArray(),
+  TITLE: getTextArray('Объявление №'),
+  DESCRIPTION: getTextArray('Описание №'),
+  TYPES: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+  CHECK: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  PHOTOS: [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  ],
+};
+
+// Генерирует объявления
+const generateOffers = (index) => {
+  const Coords = {
+    lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
+    lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
+  };
+
+  return {
+    author: {
+      avatar: Offers.AVATAR[index],
+    },
+    offer: {
+      title: Offers.TITLE[index],
+      address: `${Coords.lat}, ${Coords.lng}`,
+      price: getRandomPositiveInteger(0, 10000),
+      type: getRandomValueFromArray(Offers.TYPES),
+      rooms: getRandomPositiveInteger(1, 100),
+      guests: getRandomPositiveInteger(1, 20),
+      checkin: getRandomValueFromArray(Offers.CHECK),
+      checkout: getRandomValueFromArray(Offers.CHECK),
+      features: getRandomArray(Offers.FEATURES),
+      description: Offers.DESCRIPTION[index],
+      photos: generateRandomArray(Offers.PHOTOS),
+    },
+    location: {
+      lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
+      lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
+    },
+  };
+};
+
+const offers = new Array(QUANTITY).fill(null).map((element, index) => generateOffers(index));
